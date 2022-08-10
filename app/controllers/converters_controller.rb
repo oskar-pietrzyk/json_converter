504d5b116407a7cc3707d57bb19c52f48292593a
“Oskar <oskar@registria.com>
2022-08-10 14:51:40 +0200

    Update

--- app/controllers/converters_controller.rb
+++ app/controllers/converters_controller.rb
@@ -7,8 +7,8 @@ class ConvertersController < ApplicationController
   end
 
   def convert
-    c = Course.create!
-    c.update(converter_json: params.dig(:course, :converter_json))
+    c = Course.create!(name: params[:name])
+    c.update(converter_json: params[:converter_json])
   end
 
   def download
@@ -16,22 +16,36 @@ class ConvertersController < ApplicationController
     json = JSON.parse(course.converter_json.file.read)
     headers = ["title", "format", "attachments", "links", "sources", "text/context"]
 
-      binding.pry
     csv_data = CSV.generate(headers: true) do |csv|
       csv << headers
       csv << ["#{json['title']}", "Course type: #{json['course_type']}", "#{json['attachments'].count}", "", "", ""]
+      binding.pry
       json['ordered_slides'].each do |slide|
-        csv << [
-          "#{slide['published_attributes']['title']}",
-          "#{slide['published_attributes']['type']}", 
-          "", 
-          "", 
-          "", 
-          "#{slide['published_attributes']['serialized_body']}"
+        p_attr = slide['published_attributes']
+        structured_body = p_attr['structured_body']
+        csv << ["#{p_attr['title']}", "#{p_attr['type']}", "", "", "",  "#{p_attr['serialized_body']}"
         ]
+
+        if structured_body
+          csv << ["--- DETAILS ---", "", "", "", "", "",]
+
+          csv << ["TEXT", "TYPE", "", "", "", "",]
+
+          structured_body['blocks'].each do |block|
+            csv << ["#{block['text']}", "#{block['type']}", "", "", "", ""]
+          end
+
+          csv << ["SRC", "TYPE", "DATA", "", "", "",]
+
+          structured_body['entityMap'].each do |entityMap|
+            # csv << ["#{entityMap['src']}", "#{entityMap['type']}", "#{entityMap['data']}", "", "", ""]
+          end          
+        end
+
+        csv << ["--- END OF DETAILS ---", "", "", "", "", "",]
       end
     end
-  
+
     send_data csv_data, :type => 'text/csv', filename: "data-#{Date.today.to_s}.csv", disposition: 'attachment'
   end
 end
